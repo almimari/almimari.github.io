94ee3b7b0617977ed053cd01774bf8384be682ee
mari <31965849+almimari@users.noreply.github.com>
2021-01-29 02:15:11 -0500

    SITE TEST

---
+++ SITE/examples/js/jquery.js
@@ -0,0 +1,10 @@
+    $(document).ready(function(){
+        
+        $("#bt3").click(function(){
+            $('body, html').css("animation", "funtime 0.3s infinite");
+            $(this).css("background-color", "white");
+        });
+
+
+
+    });
